c3def73792e40170597f966b9d2d17a0f1f4015f
UltraQbik <no1skill@yandex.ru>
2024-03-09 20:39:02 +0300

    Replace match statement with a "switch" statement
    Match statement works just like a bunch of else if statements
    Switch statement works by directly executing the code, by using some kind of integer as an offset
    
    Made it using a list with 128 entries

--- src/emulator.py
+++ src/emulator.py
@@ -24,6 +24,14 @@ class Emulator:
         self.stack: bytearray = bytearray([0 for _ in range(256)])
         self.ports: bytearray = bytearray([0 for _ in range(256)])
 
+        # instruction switch case
+        self.instruction_set: list = [None for _ in range(128)]
+        for i in range(128):
+            if hasattr(self, f"_is_{i}"):
+                self.instruction_set[i] = getattr(self, f"_is_{i}")
+            else:
+                self.instruction_set[i] = self._is__
+
     def load_instructions(self, instructions: bytes):
         """
         Loads the instructions into the ROM (Read-Only Memory)
@@ -73,6 +81,233 @@ class Emulator:
         self.interrupt_register[1] = interrupt
         self.interrupt_register[2] = wrong
 
+    def _is_0(self, rom_cache_bus, data):
+        # NOP
+        pass
+
+    def _is_1(self, rom_cache_bus, data):
+        # LRA
+        self.acc = rom_cache_bus
+
+    def _is_2(self, rom_cache_bus, data):
+        # SRA
+        # address comes only from data (otherwise nothing will work)
+        self.cache[data + self.cache_page * 256] = self.acc
+
+    def _is_3(self, rom_cache_bus, data):
+        # CALL
+        self.stack[self.stack_pointer] = self.program_counter
+        self.stack_pointer = (self.stack_pointer + 1) & 255
+        self.program_counter = rom_cache_bus - 1
+
+    def _is_4(self, rom_cache_bus, data):
+        # RET
+        self.stack_pointer = (self.stack_pointer - 1) & 255
+        self.program_counter = self.stack[self.stack_pointer] - 1
+
+    def _is_5(self, rom_cache_bus, data):
+        # JMP
+        self.program_counter = rom_cache_bus - 1
+
+    def _is_6(self, rom_cache_bus, data):
+        # JMPP
+        if self.acc != 0:
+            self.program_counter = rom_cache_bus - 1
+
+    def _is_7(self, rom_cache_bus, data):
+        # JMPZ
+        if self.acc == 0:
+            self.program_counter = rom_cache_bus - 1
+
+    def _is_8(self, rom_cache_bus, data):
+        # JMPN
+        if (self.acc & 0b1000_0000) > 0:
+            self.program_counter = rom_cache_bus - 1
+
+    def _is_9(self, rom_cache_bus, data):
+        # JMPC
+        if self.carry_flag:
+            self.program_counter = rom_cache_bus - 1
+
+    def _is_10(self, rom_cache_bus, data):
+        # CCF
+        self.carry_flag = False
+
+    def _is_11(self, rom_cache_bus, data):
+        # LRP
+        self.acc = self.cache[self.acc + self.cache_page]
+
+    def _is_12(self, rom_cache_bus, data):
+        # CCP
+        self.cache_page = rom_cache_bus
+
+    def _is_13(self, rom_cache_bus, data):
+        # CRP
+        self.rom_page = rom_cache_bus
+
+    def _is_16(self, rom_cache_bus, data):
+        # AND
+        self.acc = self.acc & rom_cache_bus
+
+    def _is_17(self, rom_cache_bus, data):
+        # OR
+        self.acc = self.acc | rom_cache_bus
+
+    def _is_18(self, rom_cache_bus, data):
+        # XOR
+        self.acc = self.acc ^ rom_cache_bus
+
+    def _is_19(self, rom_cache_bus, data):
+        # NOT
+        self.acc = 255 - self.acc
+
+    def _is_20(self, rom_cache_bus, data):
+        # LSC
+        self.acc = self.acc << rom_cache_bus
+        self._check_carry()
+
+    def _is_21(self, rom_cache_bus, data):
+        # RSC
+        if ((self.acc >> rom_cache_bus) << rom_cache_bus) != self.acc:
+            self.carry_flag = True
+        self.acc = self.acc >> rom_cache_bus
+
+    def _is_22(self, rom_cache_bus, data):
+        # CMP
+        negative = (self.acc & 0b1000_0000) ^ (rom_cache_bus & 0b1000_0000)
+        if self.acc > rom_cache_bus:
+            self.acc = 1
+        elif self.acc == rom_cache_bus:
+            self.acc = 0
+        else:
+            self.acc = 255
+        if negative:
+            self.acc = 255 - self.acc
+
+    def _is_23(self, rom_cache_bus, data):
+        # CMPU
+        if self.acc > rom_cache_bus:
+            self.acc = 1
+        elif self.acc == rom_cache_bus:
+            self.acc = 0
+        else:
+            self.acc = 255
+
+    def _is_32(self, rom_cache_bus, data):
+        # ADC
+        self.acc = self.acc + rom_cache_bus + self.carry_flag
+        self._check_carry()
+
+    def _is_33(self, rom_cache_bus, data):
+        # SBC
+        self.acc = self.acc - rom_cache_bus - self.carry_flag
+        self._check_neg_carry()
+
+    def _is_34(self, rom_cache_bus, data):
+        # INC
+        self.acc += 1
+        self._check_carry()
+
+    def _is_35(self, rom_cache_bus, data):
+        # DEC
+        self.acc -= 1
+        self._check_neg_carry()
+
+    def _is_36(self, rom_cache_bus, data):
+        # ABS
+        if self._is_acc_neg():
+            self.acc = ((255 - self.acc) + 1) & 255
+
+    def _is_37(self, rom_cache_bus, data):
+        # MUL
+        self.acc = (self.acc * rom_cache_bus) & 255
+
+    def _is_38(self, rom_cache_bus, data):
+        # DIV
+        self.acc = self.acc // rom_cache_bus
+
+    def _is_39(self, rom_cache_bus, data):
+        # MOD
+        self.acc = self.acc % rom_cache_bus
+
+    def _is_40(self, rom_cache_bus, data):
+        # TSE
+        sin = (math.sin(self.acc / 32) + 1) / 2
+        sin = int(sin * 255)
+        self.acc = sin
+
+    def _is_41(self, rom_cache_bus, data):
+        # TCE
+        cos = (math.cos(self.acc / 32) + 1) / 2
+        cos = int(cos * 255)
+        self.acc = cos
+
+    def _is_42(self, rom_cache_bus, data):
+        # ADD
+        self.acc = self.acc + rom_cache_bus
+        self._check_carry()
+
+    def _is_43(self, rom_cache_bus, data):
+        # SUB
+        self.acc = self.acc - rom_cache_bus
+        self._check_neg_carry()
+
+    def _is_44(self, rom_cache_bus, data):
+        # RPL
+        if rom_cache_bus == 0:
+            self.acc = 255
+        else:
+            self.acc = int(255 / rom_cache_bus)
+
+    def _is_45(self, rom_cache_bus, data):
+        # MULH
+        self.acc = ((self.acc * rom_cache_bus) & 0b1111_1111_0000_0000) >> 8
+
+    def _is_48(self, rom_cache_bus, data):
+        # UI
+        user = input("> ")
+        try:
+            self.acc = int(user)
+        except ValueError:
+            self.acc = ord(user[0])
+        except IndexError:
+            self.acc = 0
+
+    def _is_49(self, rom_cache_bus, data):
+        # UO
+        print(self.acc)
+
+    def _is_50(self, rom_cache_bus, data):
+        # UOC
+        print(chr(self.acc), end="")
+
+    def _is_51(self, rom_cache_bus, data):
+        # UOCR
+        print(chr(self.acc))
+
+    def _is_112(self, rom_cache_bus, data):
+        # PRW
+        self.ports[data] = self.acc
+
+    def _is_113(self, rom_cache_bus, data):
+        # PRR
+        self.acc = self.ports[data]
+
+    def _is_126(self, rom_cache_bus, data):
+        # INT
+        self._set_interrupt_register(interrupt=True)
+        self.program_counter += 1
+        raise StopIteration
+
+    def _is_127(self, rom_cache_bus, data):
+        # HALT
+        self._set_interrupt_register(is_halted=True)
+        raise StopIteration
+
+    def _is__(self, rom_cache_bus, data):
+        self._set_interrupt_register(wrong=True)
+        raise StopIteration
+
     def execute_step(self):
         """
         Executes one step of the CPU
@@ -97,195 +332,7 @@ class Emulator:
             rom_cache_bus = data
 
         # execute instruction
-        match opcode:
-            case 0:
-                # NOP
-                pass
-            case 1:
-                # LRA
-                self.acc = rom_cache_bus
-            case 2:
-                # SRA
-                # address comes only from data (otherwise nothing will work)
-                self.cache[data + self.cache_page * 256] = self.acc
-            case 3:
-                # CALL
-                self.stack[self.stack_pointer] = self.program_counter
-                self.stack_pointer = (self.stack_pointer + 1) & 255
-                self.program_counter = rom_cache_bus - 1
-            case 4:
-                # RET
-                self.stack_pointer = (self.stack_pointer - 1) & 255
-                self.program_counter = self.stack[self.stack_pointer] - 1
-            case 5:
-                # JMP
-                self.program_counter = rom_cache_bus - 1
-            case 6:
-                # JMPP
-                if self.acc != 0:
-                    self.program_counter = rom_cache_bus - 1
-            case 7:
-                # JMPZ
-                if self.acc == 0:
-                    self.program_counter = rom_cache_bus - 1
-            case 8:
-                # JMPN
-                if (self.acc & 0b1000_0000) > 0:
-                    self.program_counter = rom_cache_bus - 1
-            case 9:
-                # JMPC
-                if self.carry_flag:
-                    self.program_counter = rom_cache_bus - 1
-            case 10:
-                # CCF
-                self.carry_flag = False
-            case 11:
-                # LRP
-                self.acc = self.cache[self.acc + self.cache_page]
-            case 12:
-                # CCP
-                self.cache_page = rom_cache_bus
-            case 13:
-                # CRP
-                self.rom_page = rom_cache_bus
-
-            case 16:
-                # AND
-                self.acc = self.acc & rom_cache_bus
-            case 17:
-                # OR
-                self.acc = self.acc | rom_cache_bus
-            case 18:
-                # XOR
-                self.acc = self.acc ^ rom_cache_bus
-            case 19:
-                # NOT
-                self.acc = 255 - self.acc
-            case 20:
-                # LSC
-                self.acc = self.acc << rom_cache_bus
-                self._check_carry()
-            case 21:
-                # RSC
-                if ((self.acc >> rom_cache_bus) << rom_cache_bus) != self.acc:
-                    self.carry_flag = True
-                self.acc = self.acc >> rom_cache_bus
-            case 22:
-                # CMP
-                negative = (self.acc & 0b1000_0000) ^ (rom_cache_bus & 0b1000_0000)
-                if self.acc > rom_cache_bus:
-                    self.acc = 1
-                elif self.acc == rom_cache_bus:
-                    self.acc = 0
-                else:
-                    self.acc = 255
-                if negative:
-                    self.acc = 255 - self.acc
-            case 23:
-                # CMPU
-                if self.acc > rom_cache_bus:
-                    self.acc = 1
-                elif self.acc == rom_cache_bus:
-                    self.acc = 0
-                else:
-                    self.acc = 255
-
-            case 32:
-                # ADC
-                self.acc = self.acc + rom_cache_bus + self.carry_flag
-                self._check_carry()
-            case 33:
-                # SBC
-                self.acc = self.acc - rom_cache_bus - self.carry_flag
-                self._check_neg_carry()
-            case 34:
-                # INC
-                self.acc += 1
-                self._check_carry()
-            case 35:
-                # DEC
-                self.acc -= 1
-                self._check_neg_carry()
-            case 36:
-                # ABS
-                if self._is_acc_neg():
-                    self.acc = ((255 - self.acc) + 1) & 255
-            case 37:
-                # MUL
-                self.acc = (self.acc * rom_cache_bus) & 255
-            case 38:
-                # DIV
-                self.acc = self.acc // rom_cache_bus
-            case 39:
-                # MOD
-                self.acc = self.acc % rom_cache_bus
-            case 40:
-                # TSE
-                sin = (math.sin(self.acc / 32) + 1) / 2
-                sin = int(sin * 255)
-                self.acc = sin
-            case 41:
-                # TCE
-                cos = (math.cos(self.acc / 32) + 1) / 2
-                cos = int(cos * 255)
-                self.acc = cos
-            case 42:
-                # ADD
-                self.acc = self.acc + rom_cache_bus
-                self._check_carry()
-            case 43:
-                # SUB
-                self.acc = self.acc - rom_cache_bus
-                self._check_neg_carry()
-            case 44:
-                # RPL
-                if rom_cache_bus == 0:
-                    self.acc = 255
-                else:
-                    self.acc = int(255 / rom_cache_bus)
-            case 45:
-                # MULH
-                self.acc = ((self.acc * rom_cache_bus) & 0b1111_1111_0000_0000) >> 8
-
-            case 48:
-                # UI
-                user = input("> ")
-                try:
-                    self.acc = int(user)
-                except ValueError:
-                    self.acc = ord(user[0])
-                except IndexError:
-                    self.acc = 0
-            case 49:
-                # UO
-                print(self.acc)
-            case 50:
-                # UOC
-                print(chr(self.acc), end="")
-            case 51:
-                # UOCR
-                print(chr(self.acc))
-
-            case 112:
-                # PRW
-                self.ports[data] = self.acc
-            case 113:
-                # PRR
-                self.acc = self.ports[data]
-
-            case 126:
-                # INT
-                self._set_interrupt_register(interrupt=True)
-                self.program_counter += 1
-                raise StopIteration
-            case 127:
-                # HALT
-                self._set_interrupt_register(is_halted=True)
-                raise StopIteration
-
-            case _:
-                self._set_interrupt_register(wrong=True)
-                raise StopIteration
+        self.instruction_set[opcode](rom_cache_bus, data)
 
         # increment the program counter
         self.program_counter += 1
