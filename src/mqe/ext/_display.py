2fafb66fbfcb9a658a706dd04a49a70924bfcf0f
UltraQbik <no1skill@yandex.ru>
2024-03-31 19:32:31 +0300

    fix the "infinite expand" mode for canvas

--- src/mqe/ext/_display.py
+++ src/mqe/ext/_display.py
@@ -6,6 +6,37 @@ Simple display manager, which uses tkinter to function.
 """
 
 
+class ResizingCanvas(Canvas):
+    """
+    Kindly stolen from stackoverflow class for resizable canvas :>
+    link to answer: https://stackoverflow.com/a/22837522
+    """
+    def __init__(self, parent, **kwargs):
+        kwargs["highlightthickness"] = 0
+
+        Canvas.__init__(self, parent, **kwargs)
+
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self, event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+
+        print(event.width, event.height)
+
+        self.width = event.width
+        self.height = event.height
+
+        # resize the canvas
+        self.config(width=self.width, height=self.height)
+
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all", 0, 0, wscale, hscale)
+
+
 class DisplayManager:
     """
     The display manager class, uses interrupt operations 1 and 2.
@@ -42,7 +73,7 @@ class DisplayManager:
             cls.ROOT.title("DisplayManager (page mode)")
 
         # make canvas
-        canvas = Canvas(cls.ROOT, width=cls.WINDOW_WIDTH, height=cls.WINDOW_HEIGHT, bg="#000000")
+        canvas = ResizingCanvas(cls.ROOT, width=cls.WINDOW_WIDTH, height=cls.WINDOW_HEIGHT, bg="#000000")
         canvas.pack(expand=True)
 
         # create window image buffer and put it on canvas
